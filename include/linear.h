0f6a63bd8e035a9d250560a8a69dbb3635015147
rubby0118 <41471214h@gapps.ntnu.edu.tw>
2025-12-09 18:56:12 +0800

    Add matrix operations to linear.h

--- include/linear.h
+++ include/linear.h
@@ -1,1 +1,15 @@
+#ifndef MATRIX_H
+#define MATRIX_H
 
+#define N 3   // 矩陣大小，可自行修改
+
+// 矩陣相乘：C = A * B
+void multiplyMatrix(int A[N][N], int B[N][N], int C[N][N]);
+
+// 矩陣轉置：T = A^T
+void transposeMatrix(int A[N][N], int T[N][N]);
+
+// 印出矩陣
+void printMatrix(int A[N][N]);
+
+#endif
