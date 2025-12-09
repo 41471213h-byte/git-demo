514f84767cc9ccbf3fdc0247a9c2413ff4ab58ce
rubby0118 <41471214h@gapps.ntnu.edu.tw>
2025-12-09 19:02:11 +0800

    advanced.h

--- include/advanced.h
+++ include/advanced.h
@@ -1,1 +1,12 @@
+#ifndef ADVANCED_H
+#define ADVANCED_H
 
+#include "basic.h"  // 使用 Matrix3x3 型別
+
+// 計算 3x3 行列式
+float determinant(Matrix3x3 A);
+
+// 計算 3x3 伴隨矩陣（adj(A)）
+Matrix3x3 adjoint(Matrix3x3 A);
+
+#endif
